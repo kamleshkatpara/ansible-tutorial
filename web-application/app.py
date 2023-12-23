62d2f48a501a78608845a6c30d2798fe4adc9944
Kamlesh Katpara <mekamleshk@gmail.com>
2023-12-23 20:10:37 +0530

    wip

--- web-application/app.py
+++ web-application/app.py
@@ -1,12 +1,33 @@
 import os
 from flask import Flask
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'db_user'
+app.config['MYSQL_PASSWORD'] = 'Passw0rd'
+app.config['MYSQL_DB'] = 'employee_db'
+
+mysql = MySQL(app)
+
+cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
 
 @app.route("/")
 def main():
     return "Welcome!"
+
+@app.route('/read')
+def read():
+    cursor.execute("SELECT * FROM employees")
+    row = cursor.fetchone()
+    result = []
+    while row is not None:
+      result.append(row[0])
+      row = cursor.fetchone()
+
+    return ",".join(result)
 
 if __name__ == "__main__":
     app.run()
